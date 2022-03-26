c5bed4bb0408c444967cbd78380dc90d4f82ef16
Luke-6723 <luke@ichigo.uk>
2022-03-26 11:56:04 +0100

    fuck git

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const Eris = require('eris')
 const fetch = require('node-fetch')
 const AsciiTable = require('ascii-table')
-const { token, channelID, guildID, clonedRepoFolder } = require('./config.json')
-const { readFileSync, writeFileSync, writeFile } = require('fs')
+const { token, channelID, guildID } = require('./config.json')
 const bot = new Eris(token)
-const psl = require('psl')
-const createPullRequest = require('./util/createPullRequest')
-const { nanoid } = require('nanoid')
 const exec = require('child_process').exec
 
 bot.on('ready', () => {
@@ -31,51 +27,17 @@ function isMoved (output) {
 async function followDomain (url) {
   const domainLoc = await _followDomain(url)
   if (isMoved(domainLoc)) {
-    url = domainLoc.match(/([Ll]ocation:).*/gm)[0].split(' ')[1].match(/((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:[aA-zZ]+|xyz|link|aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gm)
+    url = domainLoc.match(/([Ll]ocation:).*/gm)[0].split(' ')[1].match(/(https?:\/\/)?((?:[aA-zZ0-9](?:[aA-zZ0-9-]{0,61}[aA-zZ0-9])?\.)+[aA-zZ0-9][aA-zZ0-9-]{0,61}[aA-zZ0-9])(\/?)(.*)/gm)
     return url[0]
   }
   return url.substr(0, url.length)
 }
 
-bot.on('rawWS', (event) => {
-  if (event.t === 'INTERACTION_CREATE') {
-    if (event.d.data.custom_id === 'deleteMessageAttached') {
-      bot.deleteMessage(event.d.message.channel_id, event.d.message.id).catch()
-    }
-  }
-})
-
-const errorMessage = (id) => {
-  return {
-    content: `<@${id}>
-    
-please give type of url.
-Command syntax: \`-pr <blacklist / whitelist> <url / domain> <domain or url you want to whitelist>\`
-**Only domains can be whitelisted. URLs can't be whitelisted.**`,
-    components: [
-      {
-        type: 1,
-        components: [{
-          type: 2,
-          label: '🗑️',
-          style: 4,
-          custom_id: 'deleteMessageAttached'
-        }]
-      }
-    ]
-  }
-}
-
 bot.on('messageCreate', async (msg) => {
   if (msg.author.bot) return
-<<<<<<< HEAD
-  // msg.content = msg.content.toLowerCase()
-  const contentMatch = msg.content.replace(/`/g, "").match(/((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:[aA-zZ]+|xyz|link|aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gm)
-=======
   msg.content = msg.content.toLowerCase().replace(/`/gm, '')
   const origContent = msg.content.match(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/gm)
   const contentMatch = msg.content.match(/((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:[aA-zZ]+|xyz|link|aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gm)
->>>>>>> ec7d0702b6d6ed77b03678f036acd686ad762bb5
   if (contentMatch < 1) return
   for (let i = 0; i < contentMatch.length; i++) {
     const urlOut = await followDomain(contentMatch[i])
@@ -84,7 +46,7 @@ bot.on('messageCreate', async (msg) => {
       if (msg?.channel?.guild?.id === guildID) {
         if (msg.channel.id === channelID) {
           const data = JSON.stringify({
-            message: msg.content.replace(/`/g, "")
+            message: msg.content.match(/((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:[aA-zZ]+|xyz|link|aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gm).join(' ')
           })
           let result = await fetch('https://anti-fish.bitflow.dev/check', {
             method: 'POST',
@@ -95,7 +57,6 @@ bot.on('messageCreate', async (msg) => {
             body: data
           }).catch(err => console.log(err))
           result = await result.json()
-          console.log(result)
           if (result.match) {
             let description = 'Flagged on current sources:\n```\n'
             const table = new AsciiTable()
@@ -104,102 +65,37 @@ bot.on('messageCreate', async (msg) => {
             result.matches.forEach(m => {
               table.addRow(m.domain, m.source, m.type, m.trust_rating)
             })
-
-            description += table.toString() + '\n\n'
-
-            description += 'Matched full urls (if any):'
-            result.matches.forEach(m => {
-              description += `\n${m.url}`
-            })
-
-            description += '\n\nUnmatched domains (if any):'
+            description += table.toString() + '\n\nUnmatched domains (if any):'
 
             let unmatchedDomains = 0
             const domains = result.matches.map(m => m.domain)
-            contentMatch.forEach(d => {
-              if (!domains.includes(d) && domains.includes(d.url)) {
+            origContent.forEach(d => {
+              console.log(d)
+              if (!domains.includes(d)) {
                 unmatchedDomains++
                 description += `\n${d}`
               }
             })
-            let result = await fetch('https://anti-fish.bitflow.dev/check', {
-              method: 'POST',
-              headers: {
-                'User-Agent': 'Anti-Fish-Flag-Check-Bot v0.0.1',
-                'Content-Type': 'application/json'
+
+            if (unmatchedDomains === 0) description += '\nNo unmatched domains```'
+            else description += '```'
+
+            msg.channel.createMessage({
+              message_reference: {
+                message_id: msg.id,
+                channel_id: msg.channel.id
               },
-              body: data
-            }).catch(err => console.log(err))
-            result = await result.json()
-            if (result.match) {
-              let description = 'Flagged on current sources:\n```\n'
-              const table = new AsciiTable()
-                .setHeading('domain', 'source', 'type', 'trust rating')
-
-              result.matches.forEach(m => {
-                table.addRow(m.domain, m.source, m.type, m.trust_rating)
-              })
-
-              description += table.toString() + '\n\n'
-
-              description += 'Matched full urls (if any):'
-              result.matches.forEach(m => {
-                description += `\n${m.url}`
-              })
-
-              description += '\n\nUnmatched domains (if any):'
-
-              let unmatchedDomains = 0
-              const domains = result.matches.map(m => m.domain)
-              contentMatch.forEach(d => {
-                if (!domains.includes(d) && domains.includes(d.url)) {
-                  unmatchedDomains++
-                  description += `\n${d}`
-                }
-              })
-
-              if (unmatchedDomains === 0) description += '\nNo unmatched domains```'
-              else description += '```'
-
+              content: `**Domain is already flagged!**\n${description}`
+            })
+          } else {
+            if (contentMatch.length > 0) {
               msg.channel.createMessage({
                 message_reference: {
                   message_id: msg.id,
                   channel_id: msg.channel.id
                 },
-                components: [
-                  {
-                    type: 1,
-                    components: [{
-                      type: 2,
-                      label: '🗑️',
-                      style: 4,
-                      custom_id: 'deleteMessageAttached'
-                    }]
-                  }
-                ],
-                content: `**Domain is already flagged!**\n${description}`
+                content: `Parsed unmatched domains:\n\`\`\`\n${contentMatch.map(m => m.match(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/gm)).join('\n')}\n\nDomains followed to:\n${urlOut} \`\`\``
               })
-            } else {
-              if (contentMatch.length > 0) {
-                msg.channel.createMessage({
-                  message_reference: {
-                    message_id: msg.id,
-                    channel_id: msg.channel.id
-                  },
-                  components: [
-                    {
-                      type: 1,
-                      components: [{
-                        type: 2,
-                        label: '🗑️',
-                        style: 4,
-                        custom_id: 'deleteMessageAttached'
-                      }]
-                    }
-                  ],
-                  content: `Parsed unmatched domains:\n\`\`\`\n${contentMatch.map(m => m.match(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/gm)).join('\n')}\n\nDomains followed to:\n${urlOut} \`\`\``
-                })
-              }
             }
           }
         }
@@ -209,3 +105,4 @@ bot.on('messageCreate', async (msg) => {
 })
 
 bot.connect()
+
